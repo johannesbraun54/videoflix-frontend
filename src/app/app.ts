9a21fec74e63814c0154d7c2787c68ae5043be61
johannesbraun54 <johannesbraun02@gmail.com>
2025-06-13 17:32:14 +0200

    created landing page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
+import { LandingComponent } from './landing-component/landing-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, LandingComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
