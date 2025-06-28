eb3383db7a440d15f78bbdcb41545300c2219224
johannesbraun54 <johannesbraun02@gmail.com>
2025-06-28 13:16:13 +0200

    added input style, first sign-up logic

--- src/app/landing-component/landing-component.ts
+++ src/app/landing-component/landing-component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-landing-component',
-  imports: [RouterModule],
+  imports: [RouterModule, FormsModule],
   templateUrl: './landing-component.html',
   styleUrl: './landing-component.scss'
 })
 export class LandingComponent {
+  
+  emailForSignUp: string = '';
+  
+  constructor() { 
+    // Initialization logic can go here if needed
+  }
 
+  routeToSignUp() {
+    console.log('Sign Up clicked with email:', this.emailForSignUp);
+  }
 }
